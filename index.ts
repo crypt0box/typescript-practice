98d49bedd2db3a03646973c427ccef748d77d17c
crypt0box <pray.happy76@gmail.com>
2020-09-23 21:28:35 +0900

    initial commit

--- index.ts
+++ index.ts
@@ -5,3 +5,14 @@ let negative: number = -0.12;
 let single: string = 'hello';
 let double: string = "hello";
 let back: string = `hello`;
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: 'Jack',
+  age: 21
+}
+
+const fruits: any[] = ['Apple', 'Banana', 'Grape', 1]
+const family = ['taro', 'ziro', 32]
