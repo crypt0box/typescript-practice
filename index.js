32edf71343e459b8560d8eaf4e3df807499cc52e
crypt0box <pray.happy76@gmail.com>
2020-09-24 21:02:33 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var hasValue = true;
+var count = 10;
+var float = 3.14;
+var negative = -0.12;
+var single = 'hello';
+var double = "hello";
+var back = "hello";
+var person = {
+    name: 'Jack',
+    age: 21
+};
+var fruits = ['Apple', 'Banana', 'Grape', 1];
+var family = ['taro', 'ziro', 32, true];
+var book = ['business', 1500, false];
+book.push(21);
+console.log(person);
--- index.ts
+++ index.ts
@@ -15,4 +15,43 @@ const person: {
 }
 
 const fruits: any[] = ['Apple', 'Banana', 'Grape', 1]
-const family = ['taro', 'ziro', 32]
+const family = ['taro', 'ziro', 32, true]
+
+const book: [string, number, boolean] = ['business', 1500, false]
+book.push(21)
+
+console.log(person)
+
+enum CoffeeSize {
+  SHORT = 'SHORT',
+  TALL = 'TALL',
+  GRANDE = 'GRANDE',
+  VENTI = 'VENTI'
+}
+
+/*
+enum CoffeeSize {
+  SHORT,
+  TALL = 100
+  GRANDE,
+  VENTI,
+}
+*/
+
+const coffee = {
+  hot: true,
+  size: CoffeeSize.TALL
+}
+coffee.size = CoffeeSize.SHORT;
+
+let anything: any = true;
+anything = 'hello';
+anything = ['hello', 33, true];
+anything = {};
+anything.foo = 'bar';
+let banana = 'banana';
+banana = anything; // String型で定義していてもany型を代入できる -> なるべくanyは使わない
+
+let unionType: number | string = 10;
+unionType = 'Hello';
+unionType.toUpperCase();
