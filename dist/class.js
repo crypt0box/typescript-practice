fa0b6b788c2389703c5b066acffb5fc4541ba5f6
crypt0box <pray.happy76@gmail.com>
2020-10-15 21:24:21 +0900

    ADD: initial commit

--- dist/class.js
+++ dist/class.js
@@ -1,16 +1,24 @@
 "use strict";
 class Person {
-    constructor(initName) {
+    constructor(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
+    }
+    // greeting(this: { name: string }) {
+    //   console.log(`Hello! My name is ${this.name}`);
+    // }
+    incrementAge() {
+        this.age += 1;
     }
     greeting() {
-        console.log(`Hello! My name is ${this.name}`);
+        console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
     }
 }
-const quill = new Person('Quill');
+const quill = new Person('Quill', 38);
 quill.greeting();
-const anotherQuill = {
-    name: 'anotherQuill',
-    anotherGreeting: quill.greeting
-};
-anotherQuill.anotherGreeting(); // thisは呼び出された時に決まる
+// const anotherQuill = {
+//   name: 'anotherQuill',
+//   greeting() {},
+//   anotherGreeting: quill.greeting
+// }
+// anotherQuill.anotherGreeting(); // thisは呼び出された時に決まる
--- src/class.ts
+++ src/class.ts
@@ -1,19 +1,30 @@
 class Person {
 
   name: string;
+  private age: number;
 
-  constructor(initName: string) {
+  constructor(initName: string, initAge: number) {
     this.name = initName;
+    this.age = initAge;
   }
 
-  greeting(this: { name: string }) {
-    console.log(`Hello! My name is ${this.name}`);
+  // greeting(this: { name: string }) {
+  //   console.log(`Hello! My name is ${this.name}`);
+  // }
+
+  incrementAge() {
+    this.age += 1;
+  }
+  greeting(this: Person) {
+    console.log(`Hello! My name is ${this.name}. I am ${this.age} years old.`);
   }
 }
-const quill = new Person('Quill');
+const quill = new Person('Quill', 38);
 quill.greeting();
-const anotherQuill = {
-  name: 'anotherQuill',
-  anotherGreeting: quill.greeting
-}
-anotherQuill.anotherGreeting(); // thisは呼び出された時に決まる
+
+// const anotherQuill = {
+//   name: 'anotherQuill',
+//   greeting() {},
+//   anotherGreeting: quill.greeting
+// }
+// anotherQuill.anotherGreeting(); // thisは呼び出された時に決まる
