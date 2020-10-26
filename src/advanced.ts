44f941bffe29c13031e5fda68e9f760b38ab1b66
crypt0box <pray.happy76@gmail.com>
2020-10-26 21:36:36 +0900

    ADD: initial commit

--- src/advanced.ts
+++ src/advanced.ts
@@ -33,11 +33,13 @@ function describeProfile(nomadWorker: NomadWorker) {
   }
 }
 class Dog {
+  kind: 'dog' = 'dog';
   speak() {
     console.log('bow-wow');
   }
 }
 class Bird {
+  kind: 'bird' = 'bird';
   speak() {
     console.log('tweet-tweet');
   }
@@ -48,6 +50,10 @@ class Bird {
 type Pet = Dog | Bird;
 function havePet(pet: Pet) {
   pet.speak();
+  switch (pet.kind) {
+    case 'bird':
+      pet.fly();
+  }
   if (pet instanceof Bird) {
     pet.fly();
   }
