679a380c06d9c3ccc1e38222e9e9ad531399a3a2
crypt0box <pray.happy76@gmail.com>
2020-11-06 18:16:17 +0900

    initial commit

---
+++ src/decorator.ts
@@ -0,0 +1,25 @@
+function Logging(message: string) {
+  return function Logging(constructor: Function) {
+    console.log(message);
+    console.log(constructor);
+  }
+}
+function Component(template: string, selector: string) {
+  return function (constructor: { new(...args: any[]): { name: string } }) {
+    const mountedElement = document.querySelector(selector);
+    const instance = new constructor();
+    if (mountedElement) {
+      mountedElement.innerHTML = template;
+      mountedElement.querySelector('h1')!.textContent = instance.name;
+    }
+  }
+}
+
+@Component('<h1>{{ name }}</h1>', '#app')
+@Logging('Logging User')  // デコレータはclassの定義時に実行される
+class User {
+  name = 'Quill';
+  constructor() {
+    console.log('User was created!');
+  }
+}
