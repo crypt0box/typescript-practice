8d2812ae7017731cb8ece3e6ee83aff24610d600
crypt0box <pray.happy76@gmail.com>
2020-12-17 20:26:42 +0900

    ADD: initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
 const path = require('path');
 module.exports = {
-  entry: './dist/food-app/main.js',
+  entry: './src/food-app/main.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   }
 }
