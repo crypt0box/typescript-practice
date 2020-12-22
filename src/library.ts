10f9d7394c43ca30c9967719b061f4434ac48383
crypt0box <pray.happy76@gmail.com>
2020-12-22 20:36:09 +0900

    ADD: initial commit

--- src/library.ts
+++ src/library.ts
@@ -1,2 +1,10 @@
 // import axios from 'axios';
-import _ from 'lodash';
+// import _ from 'lodash';
+namespace myApp {
+  const hello = 'hello in namespace';
+  export const name = 'Quill';
+  export interface Nameable {
+    name: string;
+  }
+}
+let nameable: myApp.Nameable;
